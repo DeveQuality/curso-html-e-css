e4cd8c5864e7eed63e07571b0b3605be614f3352
Bernardo AS <fmacuacua116@gmail.com>
2025-09-14 20:19:17 -0700

    actualizei

---
+++ Modulo 4/MidiaQueries/Menu-responsivo/javascript.js
@@ -0,0 +1,15 @@
+function mudoutamaho(){
+    if(window.innerWidth >= 768){
+        item.style.display ='block'
+    }else{
+        intem.style.display ='none'
+    } 
+}
+   
+function aparecer(){
+    if (item.style.display == 'block'){
+        item.style.display = 'none'
+    }else{
+        item.style.display = 'block'
+    }
+ }
